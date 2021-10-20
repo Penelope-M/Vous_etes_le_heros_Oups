a7848ad38f647076a6aa03c58f265ff0a90880c3
Pénélope <Penelopemorrisson1999@gmail.com>
2021-10-20 16:29:34 -0400

    Revert "3.1"
    This reverts commit bdbe59d4487a32919280e7be03f5f2dad2e44dc1.

--- script.js
+++ script.js
@@ -440,6 +440,6 @@ const chaptersObj = {
 }
 function goToChapter(chapterName){
     const chapter=chaptersObj[chapterName];
-    document.querySelector('.chapitre').innerText=chapter.subtitle;
-    document.querySelector('.message').innerText=chapter.text;
-};
+    console.log(chapter.subtitle);
+    console.log(chapter.text);
+}
